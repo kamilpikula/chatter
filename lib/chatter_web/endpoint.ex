4b6a1ebfd278b287df9706a73774d81154ad42a2
kamilpikula <k.pikula97@gmail.com>
2020-11-05 22:38:49 +0100

    fix: set up of Wallaby

--- lib/chatter_web/endpoint.ex
+++ lib/chatter_web/endpoint.ex
@@ -1,6 +1,10 @@
 defmodule ChatterWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :chatter
 
+  if Application.get_env(:chatter, :sql_sandbox) do
+    plug Phoenix.Ecto.SQL.Sandbox
+  end
+
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
   # Set :encryption_salt if you would also like to encrypt it.
@@ -51,8 +55,4 @@ defmodule ChatterWeb.Endpoint do
   plug Plug.Head
   plug Plug.Session, @session_options
   plug ChatterWeb.Router
-
-  if Application.get_env(:chatter, :sandbox) do
-    plug Phoenix.Ecto.SQL.Sandbox
-  end
 end
